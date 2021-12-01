117f2fa2f5cd30f512683e03f4166656d9280206
MrTwinkie1812 <94935761+MrTwinkie1812@users.noreply.github.com>
2021-12-01 14:39:26 -0800

    your

--- task1.py
+++ task1.py
@@ -1,13 +1,16 @@
-#! python3
-"""
-Ask the user for their name and their email address.
-You will need to use the .strip() method for this assignment. Be aware of your 
-(2 points)
-
-Inputs:
- name
- email
-
-Sample output:
- Your name is Joe Lunchbox, and your email is joe@koolsandwiches.org.
-"""
+##### Task 1
+#Ask the user for their name and their email address.\
+#Display the output back to the user in the following format:\
+#You will need to use the .strip() method for this assignment. Be aware of your punctuation!
+#Your name is Joe Lunchbox, and your email is joe@koolsandwiches.org.
+#x = int(x)    # converts x into an integer
+#x = str(x)    # converts x into a string
+#x = float(x)  # converts x into a float
+#a = int(b)    # converts b into an integer and stores it into a new variable, a
+world = input ("enter name")
+world2 = input ("enter email address")
+x = world.strip ()
+y = world2.strip ()
+x = "Joe Lunchbox,"
+y = "joe@koolsandwiches/.org"
+print(f"your name is {x} and your email address {y}")
