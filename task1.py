ba046812e60d1d68262752fceb7c7ce94be93aad
MrTwinkie1812 <94935761+MrTwinkie1812@users.noreply.github.com>
2022-01-21 10:16:43 -0800

    commit

--- task1.py
+++ task1.py
@@ -17,8 +17,9 @@ x = "Joe Lunchbox,"
 y = "joe@koolsandwiches/.org"
 print(f"Your name is {x} and your email is {y}.")
 """ 
-txt2 = input("enter email address")
+
 txt = input("enter name")
+txt2 = input("enter email address")
 
 x = txt.strip() + (",")
 y = txt2.strip() +  (".")
